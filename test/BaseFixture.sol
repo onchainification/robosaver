b38c264961b9588174cfae5a4190f56e2466fc2e
Petrovska <petrovska@badger.com>
2024-04-05 21:42:08 +0700

    test: leverage mint capabilities to override storage eure layout properly

--- test/BaseFixture.sol
+++ test/BaseFixture.sol
@@ -1,15 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-import {Test, console} from "forge-std/Test.sol";
+import "forge-std/Test.sol";
 
 import {Delay} from "delay-module/Delay.sol";
 
 import {RoboSaverModule} from "../src/RoboSaverModule.sol";
 
 import {ISafe} from "../src/interfaces/safe/ISafe.sol";
+import {IEURe} from "../src/interfaces/eure/IEURe.sol";
 
 contract BaseFixture is Test {
+    using stdStorage for StdStorage;
+
     /*//////////////////////////////////////////////////////////////////////////
                                    CONSTANTS
     //////////////////////////////////////////////////////////////////////////*/
@@ -19,6 +22,8 @@ contract BaseFixture is Test {
 
     address constant EUR_E = 0xcB444e90D8198415266c6a2724b7900fb12FC56E;
     address constant WETH = 0x6A023CCd1ff6F2045C3309768eAd9E68F978f6e1;
+
+    address constant EUR_E_MINTER = 0x882145B1F9764372125861727d7bE616c84010Ef;
 
     address constant GNOSIS_SAFE = 0xa4A4a4879dCD3289312884e9eC74Ed37f9a92a55;
 
@@ -40,6 +45,9 @@ contract BaseFixture is Test {
 
         vm.prank(GNOSIS_SAFE);
         safe.enableModule(address(delayModule));
+
+        vm.prank(EUR_E_MINTER);
+        IEURe(EUR_E).mintTo(GNOSIS_SAFE, 100e18);
 
         vm.label(EUR_E, "EUR_E");
         vm.label(WETH, "WETH");
--- test/TransferErc20Test.sol
+++ test/TransferErc20Test.sol
@@ -4,11 +4,12 @@ pragma solidity ^0.8.17;
 import {BaseFixture} from "./BaseFixture.sol";
 
 import {IERC20} from "@oz/token/ERC20/IERC20.sol";
+
 import {Enum} from "../lib/delay-module/node_modules/@gnosis.pm/safe-contracts/contracts/common/Enum.sol";
 
 contract TransferErc20Test is BaseFixture {
     function test_transferErc20() public {
-        uint256 tokenAmount = 10e18;
+        uint256 tokenAmount = 1e18;
 
         roboModule.transferErc20(EUR_E, tokenAmount, WETH);
 
@@ -16,8 +17,10 @@ contract TransferErc20Test is BaseFixture {
 
         bytes memory payload = abi.encodeWithSignature("transfer(address,uint256)", WETH, tokenAmount);
 
-        vm.mockCall(EUR_E, abi.encodeWithSelector(IERC20.balanceOf.selector, GNOSIS_SAFE), abi.encode(tokenAmount));
+        assertEq(IERC20(EUR_E).balanceOf(WETH), 0);
 
         delayModule.executeNextTx(EUR_E, 0, payload, Enum.Operation.Call);
+
+        assertEq(IERC20(EUR_E).balanceOf(WETH), tokenAmount);
     }
 }
