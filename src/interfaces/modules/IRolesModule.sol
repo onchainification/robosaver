fb248b2a8186423b2f3e2f8bb6b4693fdd6b7450
Gosuto Inzasheru <gosuto@inzasheru.email>
2024-04-06 13:28:15 +0200

    fix: interface name

--- src/interfaces/modules/IRolesModule.sol
+++ src/interfaces/modules/IRolesModule.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 pragma solidity ^0.8.4;
 
-interface Roles {
+interface IRolesModule {
     type ExecutionOptions is uint8;
     type Operation is uint8;
     type Operator is uint8;
