6bf92cb1d7533a0781a4348e86ceb46bcf04e199
Petrovska <petrovska@badger.com>
2024-04-17 23:12:12 +0700

    feat: introduce `bptTopup` method for siphoning eure into bpt

--- src/RoboSaverVirtualModule.sol
+++ src/RoboSaverVirtualModule.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
+import {IMulticall} from "@gnosispay-kit/interfaces/IMulticall.sol";
 import {IRolesModifier} from "@gnosispay-kit/interfaces/IRolesModifier.sol";
 import {IDelayModifier} from "@gnosispay-kit/interfaces/IDelayModifier.sol";
 import {IAsset} from "@balancer-v2/interfaces/contracts/vault/IAsset.sol";
@@ -12,6 +13,11 @@ contract RoboSaverVirtualModule {
     /*//////////////////////////////////////////////////////////////////////////
                                    CONSTANTS
     //////////////////////////////////////////////////////////////////////////*/
+
+    uint256 constant SLIPP = 9_800;
+    uint256 constant MAX_BPS = 10_000;
+
+    address constant MULTICALL_V3 = 0xcA11bde05977b3631167028862bE2a173976CA11;
 
     IERC20 constant EURE = IERC20(0xcB444e90D8198415266c6a2724b7900fb12FC56E);
     IERC20 constant STEUR = IERC20(0x004626A008B1aCdC4c74ab51644093b155e59A23);
@@ -42,6 +48,7 @@ contract RoboSaverVirtualModule {
     //////////////////////////////////////////////////////////////////////////*/
 
     event SafeTopup(address indexed safe, uint256 amount, uint256 timestamp);
+    event BptTopup(address indexed safe, uint256 amount, uint256 timestamp);
 
     /*//////////////////////////////////////////////////////////////////////////
                                      CONSTRUCTOR
@@ -93,6 +100,8 @@ contract RoboSaverVirtualModule {
     }
 
     /// @notice siphon eure out of the bpt pool
+    /// @param _avatar The address of the avatar in which the virtual module is withdrawing in behalf of.
+    /// @param _topupAmount The amount of eure to withdraw from the bpt pool.
     function safeTopup(address _avatar, uint256 _topupAmount)
         external
         onlyTopupAgents
@@ -112,9 +121,8 @@ contract RoboSaverVirtualModule {
         /// [BPT_IN_FOR_EXACT_TOKENS_OUT, amountsOut, maxBPTAmountIn]
         uint256[] memory amountsOut = new uint256[](2);
         amountsOut[1] = _topupAmount;
-        bytes memory userData = abi.encode(
-            StablePoolUserData.ExitKind.BPT_IN_FOR_EXACT_TOKENS_OUT, amountsOut, BPT_EURE_STEUR.balanceOf(_avatar)
-        );
+        bytes memory userData =
+            abi.encode(StablePoolUserData.ExitKind.BPT_IN_FOR_EXACT_TOKENS_OUT, amountsOut, type(uint256).max);
 
         request_ = IVault.ExitPoolRequest(assets, minAmountsOut, userData, false);
 
@@ -125,6 +133,52 @@ contract RoboSaverVirtualModule {
         delayModule.execTransactionFromModule(address(BALANCER_VAULT), 0, payload, 0);
 
         emit SafeTopup(_avatar, _topupAmount, block.timestamp);
+    }
+
+    /// @notice siphon eure into the bpt pool
+    /// @param _avatar The address of the avatar in which the virtual module is depositing in behalf of.
+    /// @param _excessEureFunds The amount of eure to deposit into the bpt pool.
+    function bptTopup(address _avatar, uint256 _excessEureFunds)
+        external
+        onlyTopupAgents
+        returns (IMulticall.Call3[] memory calls_)
+    {
+        // 1. approval of eure
+        bytes memory approvalPayload =
+            abi.encodeWithSignature("approve(address,uint256)", address(BALANCER_VAULT), _excessEureFunds);
+
+        // 2. join bpt
+        IAsset[] memory assets = new IAsset[](3);
+        assets[0] = IAsset(address(STEUR));
+        assets[1] = IAsset(address(BPT_EURE_STEUR));
+        assets[2] = IAsset(address(EURE));
+
+        uint256[] memory maxAmountsIn = new uint256[](3);
+        maxAmountsIn[2] = _excessEureFunds;
+
+        // ['uint256', 'uint256[]', 'uint256']
+        // [EXACT_TOKENS_IN_FOR_BPT_OUT, amountsIn, minimumBPT]
+        uint256[] memory amountsIn = new uint256[](2);
+        amountsIn[1] = _excessEureFunds;
+        uint256 minimumBPT = (_excessEureFunds * SLIPP) / MAX_BPS;
+        bytes memory userData =
+            abi.encode(StablePoolUserData.JoinKind.EXACT_TOKENS_IN_FOR_BPT_OUT, amountsIn, minimumBPT);
+
+        IVault.JoinPoolRequest memory request = IVault.JoinPoolRequest(assets, maxAmountsIn, userData, false);
+
+        bytes memory joinPoolPayload =
+            abi.encodeWithSelector(IVault.joinPool.selector, BPT_EURE_STEUR_POOL_ID, _avatar, _avatar, request);
+
+        // 3. batch approval and join into a multicall
+        calls_ = new IMulticall.Call3[](2);
+        calls_[0] = IMulticall.Call3(address(EURE), false, approvalPayload);
+        calls_[1] = IMulticall.Call3(address(BALANCER_VAULT), false, joinPoolPayload);
+
+        bytes memory multiCallPayalod = abi.encodeWithSelector(IMulticall.aggregate3.selector, calls_);
+
+        delayModule.execTransactionFromModule(MULTICALL_V3, 0, multiCallPayalod, 0);
+
+        emit BptTopup(_avatar, _excessEureFunds, block.timestamp);
     }
 
     function execQueuedTransaction() external onlyTopupAgents {
