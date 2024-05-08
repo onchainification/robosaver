553bfafd3cdffd21471efd224f3da664fea1d360
Gosuto Inzasheru <gosuto@inzasheru.email>
2024-05-08 21:15:40 +0400

    chore: bump solidity pragma

--- src/RoboSaverVirtualModule.sol
+++ src/RoboSaverVirtualModule.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.25;
 
 import {IMulticall} from "@gnosispay-kit/interfaces/IMulticall.sol";
 import {IRolesModifier} from "@gnosispay-kit/interfaces/IRolesModifier.sol";
