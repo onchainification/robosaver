4df3ad8a0cb72a3d0ae760ebd4d06f451b1f67a3
Petrovska <petrovska@badger.com>
2024-05-22 21:11:47 +0900

    feat: add check in `checker` to verify no pendant tx is queued on the delay module

---
+++ test/CheckerTest.t.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+import "forge-std/Test.sol";
+
+import {BaseFixture} from "./BaseFixture.sol";
+
+import "@balancer-v2/interfaces/contracts/vault/IVault.sol";
+
+import {Enum} from "../lib/delay-module/node_modules/@gnosis.pm/safe-contracts/contracts/common/Enum.sol";
+
+import {RoboSaverVirtualModule} from "../src/RoboSaverVirtualModule.sol";
+
+contract TopupTest is BaseFixture {
+    function testChecker_When_TopupIsRequired() public {
+        (bool canExec, bytes memory execPayload) = roboModule.checker();
+
+        assertFalse(canExec);
+        assertEq(execPayload, bytes("Neither deficit nor surplus; no action needed"));
+
+        uint256 tokenAmountTargetToMove = _transferOutBelowThreshold();
+
+        vm.warp(block.timestamp + COOL_DOWN_PERIOD);
+
+        bytes memory payload = abi.encodeWithSignature("transfer(address,uint256)", WETH, tokenAmountTargetToMove);
+
+        delayModule.executeNextTx(EURE, 0, payload, Enum.Operation.Call);
+
+        (canExec, execPayload) = roboModule.checker();
+
+        assertTrue(canExec, "CanExec: not executable");
+        assertEq(bytes4(execPayload), ADJUST_POOL_SELECTOR, "Selector: not adjust pool (0xba2f0056)");
+    }
+
+    function testChecker_When_TxIsOnQueue() public {
+        (bool canExec, bytes memory execPayload) = roboModule.checker();
+
+        assertFalse(canExec);
+        assertEq(execPayload, bytes("Neither deficit nor surplus; no action needed"));
+
+        // queue a tx, leverage the `_transferOutBelowThreshold` function from base fixture
+        _transferOutBelowThreshold();
+
+        (canExec, execPayload) = roboModule.checker();
+
+        assertFalse(canExec);
+        assertEq(execPayload, bytes("Transaction in queue, wait for it to be executed"));
+    }
+}
