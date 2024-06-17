2572ceac271fe1a668dcdd7399488b1984b1f788
Petrovska <petrovska@badger.com>
2024-06-17 20:45:13 +0900

    test: add assert for informative methods

---
+++ test/InformativeMethodsTest.t.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+import {BaseFixture} from "./BaseFixture.sol";
+
+import {RoboSaverVirtualModule} from "../src/RoboSaverVirtualModule.sol";
+
+/// @notice Suite is focus mainly in the following methods for coverage:
+/// - `RoboSaverVirtualModule.name()`
+/// - `RoboSaverVirtualModule.version()`
+contract InformativeMethodsTest is BaseFixture {
+    function testName() public view {
+        assertEq(roboModule.name(), "RoboSaverVirtualModule-EURE", "Name: not matching");
+    }
+
+    function testVersion() public view {
+        assertEq(roboModule.version(), "v0.0.2-alpha2", "Version: not matching");
+    }
+
+    /// @dev include here any other method which may be simply informative. e.g: assets, balances etc
+}
