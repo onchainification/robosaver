9fa49313e2d7e6d82e8517dc803e4f823353fd2a
Petrovska <petrovska@badger.com>
2024-06-20 22:56:26 +0900

    test: upkeep id name to reflect the module type, base currency and address of the card

--- test/BaseFixture.sol
+++ test/BaseFixture.sol
@@ -129,7 +129,7 @@ contract BaseFixture is Test {
         IERC20(LINK).approve(address(CL_REGISTRAR), LINK_FOR_TASK_TOP_UP);
 
         IKeeperRegistrar.RegistrationParams memory registrationParams = IKeeperRegistrar.RegistrationParams({
-            name: roboModule.name(),
+            name: string.concat(roboModule.name(), _addressToString(address(safe))),
             encryptedEmail: "",
             upkeepContract: address(roboModule),
             gasLimit: 2_000_000,
@@ -180,6 +180,22 @@ contract BaseFixture is Test {
         vm.label(address(roboModule), "ROBO_MODULE");
         vm.label(BPT_STEUR_EURE, "BPT_STEUR_EURE");
         vm.label(address(roboModule.BALANCER_VAULT()), "BALANCER_VAULT");
+    }
+
+    function _addressToString(address _addr) internal pure returns (string memory) {
+        bytes32 value = bytes32(uint256(uint160(_addr)));
+        bytes memory alphabet = "0123456789abcdef";
+
+        bytes memory str = new bytes(42);
+        str[0] = "0";
+        str[1] = "x";
+
+        for (uint256 i = 0; i < 20; i++) {
+            str[2 + i * 2] = alphabet[uint8(value[i + 12] >> 4)];
+            str[3 + i * 2] = alphabet[uint8(value[i + 12] & 0x0f)];
+        }
+
+        return string(str);
     }
 
     // ref: https://github.com/ethereum/solidity/issues/14996
