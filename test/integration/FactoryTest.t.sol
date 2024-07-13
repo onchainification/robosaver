14a81a5f5677d1adad89acf3e4360158ec29611d
Gosuto Inzasheru <gosuto@inzasheru.email>
2024-07-13 16:53:00 +0200

    test: comment out unused test for now

--- test/integration/FactoryTest.t.sol
+++ test/integration/FactoryTest.t.sol
@@ -32,26 +32,25 @@ contract FactoryTest is BaseFixture {
         vm.stopPrank();
     }
 
-    function test_RevertWhen_UpkeepReturnsZero() public {
-        bytes memory creationCode = abi.encodePacked(type(RoboSaverVirtualModule).creationCode);
-        bytes32 salt = keccak256(abi.encodePacked(address(safe)));
-        address deterministicVirtualModuleAddress = _getDeterministicAddress(creationCode, salt);
+    /// @todo pendant of implementing properly in another PR ensuring proper storage manipulation
+    // function test_RevertWhen_UpkeepReturnsZero() public {
+    //     bytes memory creationCode = abi.encodePacked(type(RoboSaverVirtualModule).creationCode);
+    //     bytes32 salt = keccak256(abi.encodePacked(address(safe)));
+    //     address deterministicVirtualModuleAddress = _getDeterministicAddress(creationCode, salt);
 
-        // seems that only `upkeepId_` could be return null in case that it is not "autoApprove"
-        // ref: https://github.com/smartcontractkit/chainlink/blob/develop/contracts/src/v0.8/automation/v2_0/KeeperRegistrar2_0.sol#L376
-        IKeeperRegistrar.RegistrationParams memory params = IKeeperRegistrar.RegistrationParams({
-            name: string.concat("RoboSaverVirtualModule-EURE", "-", _addressToString(address(safe))),
-            encryptedEmail: "",
-            upkeepContract: deterministicVirtualModuleAddress,
-            gasLimit: 2_000_000,
-            adminAddress: address(roboModuleFactory),
-            triggerType: 0,
-            checkData: "",
-            triggerConfig: "",
-            offchainConfig: "",
-            amount: 200e18
-        });
-
-        // @todo pendant of implementing properly in another PR ensuring proper storage manipulation
-    }
+    //     // seems that only `upkeepId_` could be return null in case that it is not "autoApprove"
+    //     // ref: https://github.com/smartcontractkit/chainlink/blob/develop/contracts/src/v0.8/automation/v2_0/KeeperRegistrar2_0.sol#L376
+    //     IKeeperRegistrar.RegistrationParams memory params = IKeeperRegistrar.RegistrationParams({
+    //         name: string.concat("RoboSaverVirtualModule-EURE", "-", _addressToString(address(safe))),
+    //         encryptedEmail: "",
+    //         upkeepContract: deterministicVirtualModuleAddress,
+    //         gasLimit: 2_000_000,
+    //         adminAddress: address(roboModuleFactory),
+    //         triggerType: 0,
+    //         checkData: "",
+    //         triggerConfig: "",
+    //         offchainConfig: "",
+    //         amount: 200e18
+    //     });
+    // }
 }
